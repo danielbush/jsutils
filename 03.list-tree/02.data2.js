6050fa3f40d88b169dd83112b652463e7c6cf721
Daniel Bush <dlb.id.au@gmail.com>
2013-04-24 10:02:31 +1000

    Added facet for listtree.

--- 03.list-tree/02.data2.js
+++ 03.list-tree/02.data2.js
@@ -3,6 +3,7 @@ $dlb_id_au$.utils.listtree.data2 = function() {
 
   var module = {};
   var object = $dlb_id_au$.utils.gen_utils.object;
+  var facets = $dlb_id_au$.utils.facets;
 
   module.makeEntry = function() {
     return {
@@ -14,6 +15,12 @@ $dlb_id_au$.utils.listtree.data2 = function() {
       }
     };
   };
+
+  // Create facet function for utils listtree:
+  module.list = facets.makeFacet('list',module.makeEntry);
+
+  // Standard deref function:
+  module.deref = facets.deref;
 
   return module;
 
