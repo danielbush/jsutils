805917cd4bc995c564cf921eb92ae1b6093e0cf6
Daniel Bush <dlb.id.au@gmail.com>
2013-05-28 20:15:54 +1000

    Removing more facet code.

--- 03.list-tree/02.data2.js
+++ 03.list-tree/02.data2.js
@@ -3,7 +3,6 @@ $dlb_id_au$.utils.listtree.data2 = function() {
 
   var module = {};
   var object = $dlb_id_au$.utils.gen_utils.object;
-  var facets = $dlb_id_au$.utils.facets;
 
   module.makeEntry = function() {
     return {
@@ -13,12 +12,6 @@ $dlb_id_au$.utils.listtree.data2 = function() {
       firstChild$:null
     };
   };
-
-  // Create facet function for utils listtree:
-  module.facet = facets.makeFacet2('list',module.makeEntry);
-
-  // Standard deref function:
-  module.facet.deref = facets.deref;
 
   return module;
 
