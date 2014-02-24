323aa65c857d234c1c6c9a458df7be90ce08c85a
Daniel Bush <dlb.id.au@gmail.com>
2014-02-24 21:13:18 +1100

    Added copyright.

--- 03.list-tree/02.data2.js
+++ 03.list-tree/02.data2.js
@@ -1,3 +1,11 @@
+/*
+The files in this directory are part of $dlb_id_au$.utils, a javascript-based library.
+Copyright (C) 2014 Daniel Bush
+This program is distributed under the terms of the GNU
+General Public License.  A copy of the license should be
+enclosed with this project in the file LICENSE.  If not
+see <http://www.gnu.org/licenses/>.
+*/
 
 $dlb_id_au$.utils.listtree.data2 = function() {
 
